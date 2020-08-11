560bfd29f99707bb5619cd92ed5b463f314c10c3
Brian Smith <brian.smith@coxautoinc.com>
2020-08-11 13:02:53 -0400

    Update the body key value to validate

--- test/consumer-uuid.spec.ts
+++ test/consumer-uuid.spec.ts
@@ -74,7 +74,7 @@ describe('UUID API', () => {
           headers: { 'Content-Type': 'application/json; charset=utf-8' },
           body: {
             uuid: '356c1f6a-896f-491e-ad5d-23b522961d26',
-            isValid: true
+            validate: true
           }
         }
       })
@@ -83,7 +83,7 @@ describe('UUID API', () => {
     it('return validation true', done => {
       const validBody = {
         uuid: '356c1f6a-896f-491e-ad5d-23b522961d26',
-        isValid: true
+        validate: true
       }
 
       request.get(`http://localhost:${process.env.MOCK_PORT}/api/v0/uuid/validate/356c1f6a-896f-491e-ad5d-23b522961d26`)
